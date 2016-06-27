650c04ccbf78ccc7820791d0f819aa52ed2c9fa6
Ethan Favia <efavia215@live.com>
2016-06-27 16:01:31 -0400

    logic storing values on firebase

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,7 +7,8 @@
   firebase.initializeApp(config);
 
   var db = firebase.database();
-
+  var p1db;
+  var p2db;
   $("#p1").on('click', function(){
   	$('#p1choice').show();
   	$('.start').hide();
@@ -16,10 +17,12 @@
 
   $('.p1choices').on('click', function(){
   	var p1 = $(this).attr('value');
+  	p1db = p1;
   	console.log(p1)
   	db.ref().update({
   		player1: p1,
   	})
+  	gameRun();
   	return false; 
 
   });
@@ -36,12 +39,42 @@
   		player2: p2,
   	})
   	return false; 
-
+  	gameRun();
   });
 
+ 
 
+db.ref().on('value', function(snapshot){
+	var gameChoices = snapshot.val();
+	 p2db = gameChoices.player2;
+	 p1db = gameChoices.player1;
+	console.log(gameChoices)
+	
+});
 
+ function gameRun() {
+  	if(p1db != "notChosen" && p2db != "notChosen"){
+  		if(p1db === p2db) {
+  			$("#results").text("its a tie");
+	  		if(p1db === "rock" && p2db === "scissors"){
+	  			$("#results").text("Player 1 wins");
+	  		} else {
+	  			$("#results").text("Player 2 wins");
+	  		}
+	  		if(p1db === "paper" && p2db === "rock"){
+	  			$("#results").text("Player 1 wins");
+	  		} else {
+	  			$("#results").text("Player 2 wins");
+	  		}
+	  		if(p1db === "scissors" && p2db === "paper"){
+	  			$("#results").text("Player 1 wins");
+	  		} else {
+	  			$("#results").text("Player 2 wins");
+	  		}
+  		}  
+  	}
 
+  }
   $(document).ready(function(){
   	$('#p1choice').hide();
   	$('#p2choice').hide();
