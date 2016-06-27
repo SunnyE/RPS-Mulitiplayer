2f69d5fcf7be2fba1583c288404e85448bb40d92
Ethan Favia <efavia215@live.com>
2016-06-27 14:08:04 -0400

    starting to work on JS

---
+++ assets/javascript/app.js
@@ -0,0 +1,7 @@
+  var config = {
+    apiKey: "AIzaSyB1WeSpzG_QY1bbJ1YvKEjcorbXq-761n0",
+    authDomain: "rpsmulti.firebaseapp.com",
+    databaseURL: "https://rpsmulti.firebaseio.com",
+    storageBucket: "rpsmulti.appspot.com",
+  };
+  firebase.initializeApp(config);
